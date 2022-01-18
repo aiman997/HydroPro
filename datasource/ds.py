d1d3bee21ca729801be89ae5f8e7f03a8dab2ff5
mahrous-amer <mahrous-amer@protonmail.com>
2022-01-18 20:28:36 +0800

    DS -> WEB

--- datasource/ds.py
+++ datasource/ds.py
@@ -8,15 +8,15 @@ import logging
 import aiohttp
 import asyncpg
 import aioredis
+import async_timeout
 
 URL = 'http://192.168.101.26:80'
 redis = aioredis.from_url("redis://redis", db=1)
 pubsub = redis.pubsub()
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 async def Pgfetch(query):
     conn = await asyncpg.connect(host='postgres', database='hydrodb', user='postgres', password='eamon2hussien')
-    logging.debug(query)
     result = await conn.fetch(query)
     await conn.close()
     return result
@@ -30,26 +30,38 @@ async def record(res):
 
 async def fetch():
     async with aiohttp.ClientSession() as session:
-        async with session.get(URL, data = '{"action": "waterON"}') as resp:
-            logging.debug(resp.text)
-            keyvalues = await resp.text()
-            values = re.findall("\{(.*?)\}", keyvalues)
-            result = []
-            for row in values:
-                x = row.split(',')
-                for column in x:
-                    key_value_pairs = column.split(':')
-                    result += key_value_pairs
-            res_dct = {result[i]: result[i + 1] for i in range(0, len(result), 2)}
-            return res_dct
+        try:
+            async with session.get(URL, data = '{"action": "waterON"}') as resp:
+                keyvalues = await resp.text()
+                values = re.findall("\{(.*?)\}", keyvalues)
+                result = []
+                for row in values:
+                    x = row.split(',')
+                    for column in x:
+                        key_value_pairs = column.split(':')
+                        result += key_value_pairs
+                res_dct = {result[i]: result[i + 1] for i in range(0, len(result), 2)}
+                return res_dct
+        except Exception as e:
+            logging.warning(e)
 
+async def sub():
+    await pubsub.subscribe("Plant::Controls")
+    x = await pubsub.get_message()
+    if x is None:
+        logging.info('Empty data from controls')
+    elif x.get('data') != 1:
+        logging.critical(x.get('data'))
+
+res_dct = ()
 async def main():
     while (1):
         res_dct = await fetch()
-        await redis.publish('Plant::Data', json.dumps(res_dct, indent = 4))
-        logging.debug('PUBLISHED Plant::Data ' + time.strftime("%I:%M:%S %p ", time.localtime()) + json.dumps(res_dct, indent = 4))
+        await redis.set('Plant::Data', json.dumps(res_dct))
+        logging.warning('PUBLISHED Plant::Data' + time.strftime("%I:%M:%S %p ", time.localtime()) + json.dumps(res_dct, indent = 4))
         await record(res_dct)
-        await asyncio.sleep(20)
+        await sub()
+        await asyncio.sleep(1)
 
 if __name__ == '__main__':
     asyncio.run(main())
