546536a466a1d71907d5722202302f7dabc051cf
aiman997 <aimanelkhedrawi11@gmail.com>
2022-02-09 20:42:11 +0800

    wip

--- datasource/ds.py
+++ datasource/ds.py
@@ -10,56 +10,70 @@ import asyncpg
 import aioredis
 import async_timeout
 
-URL = 'http://10.243.199.34:5000/PHon'
+URL = 'http://10.243.199.34:5000'
 redis = aioredis.from_url("redis://redis", db=1)
 pubsub = redis.pubsub()
 logging.basicConfig(level=logging.INFO)
 
 async def Pgfetch(query):
-    conn = await asyncpg.connect(host='postgres', database='hydrodb', user='postgres', password='eamon2hussien')
-    result = await conn.fetch(query)
-    await conn.close()
-    return result #to where?
+    result = None
+    while result == None:
+        try:
+            conn = await asyncpg.connect(host='postgres', database='hydrodb', user='postgres', password='eamon2hussien')
+            result = await conn.fetch(query)
+            await conn.close()
+            return result
+        except Exception as e:
+            logging.warning('FROM POSTGRES' + e)
 
 async def record(res):
-    values = 'now()'
-    for x in ('PH_S', 'PH_R', 'EC_S', 'EC_R', 'TEMP_S', 'TEMP_R', 'PUMP_S', 'ECUP_S', 'PHUP_S', 'PHDOWN_S'):
-        values +=  ', ' + res[x]
-    query = f'INSERT INTO hydro.hydrotable (TIMEZ, STATUS_PH, READING_PH, STATUS_EC, READING_EC, STATUS_TEMP, READING_TEMP, STATUS_MPUMP, STATUS_ECUP, STATUS_PHUP, STATUS_PHDOWN) VALUES ({values})'
-    await Pgfetch(query)
+    try:
+        logging.info(res)
+        values = "now()"
+        for x in ("PH_State", "PH_Reading", "EC_State", "EC_Reading", "TEMP_State", "TEMP_Reading", "WL_State", "WL_Reading", "MPUMP_State", "ECUP_State", "PHUP_State", "PHDWN_State"):
+            values +=  "," + str(res[x])
+        query = f'INSERT INTO hydro.hydrotable (TIMEZ, STATUS_PH, READING_PH, STATUS_EC, READING_EC, STATUS_TEMP, READING_TEMP,  STATUS_WLEVEL, READING_WLEVEL, STATUS_MPUMP, STATUS_ECUP, STATUS_PHUP, STATUS_PHDOWN) VALUES ({values})'
+        await Pgfetch(query)
+    except Exception as e:
+        logging.warning('ERROR WHILE RECORDING DATA: '+e)
 
-async def fetch():
+async def fetch(route):
     async with aiohttp.ClientSession() as session:
         try:
-            async with session.get(URL) as resp:
-                keyvalues = await resp.text()
-                logging.warning(resp)
-		        # logging.warning(keyvalues)
-                return keyvalues
+            resp = await session.get(URL+route)
+            return await resp.text()
         except Exception as e:
-            logging.warning(e)
+            logging.warning('ERROR WHILE FETCHING DATA: '+str(e))
 
-async def sub():
-    await pubsub.subscribe("Plant::Controls")
-    x = await pubsub.get_message()
-    if x is None:
-        logging.info('Empty data from controls')
-    elif x.get('data') != 1:
-        logging.critical(x.get('data'))
+async def appendkeys(res_dct):
+    prefix = ''
+    for prefix in res_dct.keys():
+        res = await redis.set('Plant::Data::'+prefix, json.dumps(str(res_dct[prefix])))
+        logging.info(f'Setting Plant::Data::{prefix} result {res}')
 
-res_dct = ()
+async def processdata():
+    try:
+        res_str = await fetch('/Tick')
+        res_dct = json.loads(res_str)
+        await appendkeys(res_dct)
+        await redis.set('Plant::Data', json.dumps(res_dct))
+        logging.info('PUBLISHED Plant::Data \t' + time.strftime("%I:%M:%S %p ", time.localtime()) + json.dumps(res_dct, indent=4))
+        await record(res_dct)
+    except Exception as e:
+        logging.warning('ERROR WHILE PROCESSING DATA: '+str(e))
+
 async def main():
-    logging.info('I am up...')
+    await pubsub.subscribe("Plant::Controls")
     while (1):
-        res_dct = await fetch()
-        logging.info(res_dct)
-        # await redis.set('Plant::Data', json.dumps(res_dct))
-        # logging.warning('PUBLISHED Plant::Data' + time.strftime("%I:%M:%S %p ", time.localtime()) + json.dumps(res_dct, indent = 4))
-        # await record(res_dct)
-        # await sub()
-        logging.info('Night...')
+        await processdata()
+        msg = await pubsub.get_message()
+        if msg is None:
+            logging.info('Empty data from controls')
+        elif msg.get('data') != 1:
+            msgdata = msg.get('data')
+            res_dct = await fetch(msgdata.decode())
+            logging.info(' IN FROM FLASK ' + time.strftime("%I:%M:%S %p ", time.localtime()) + json.dumps(res_dct, indent = 4))
         await asyncio.sleep(60)
-        logging.info('Done sleeping...')
 
 if __name__ == '__main__':
     asyncio.run(main())
--- web/hydro.py
+++ web/hydro.py
@@ -11,36 +11,37 @@ import asyncio
 import asyncpg
 import aioredis
 import aiohttp
-
-URL = 'http://10.243.199.34:5000'
-Path = ""
-res_dct = ()
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import random
+import numpy as np
+import io
+import base64
 
 app = Flask(__name__)
+URL = 'http://10.243.199.34:5000'
 redis = aioredis.from_url("redis://redis", db=1)
 pubsub = redis.pubsub()
-
-# Configure Logging
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.DEBUG)
-
-async def fetch(Path):
-    logging.warning("IM IN fetch")
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(URL + Path) as resp:
-                keyvalues = await resp.text()
-                logging.warning(resp)
-		        # logging.warning(keyvalues)
-                return keyvalues
-        except Exception as e:
-            logging.warning(e)
+app.logger.setLevel(logging.INFO)
 
 async def Pgfetch(query):
-    conn = await asyncpg.connect(host='postgres', database='hydrodb', user='postgres', password='eamon2hussien')
-    result = await conn.fetch(query)
-    await conn.close()
-    return result
+    result = None
+    while result == None:
+        try:
+            conn = await asyncpg.connect(host='postgres', database='hydrodb', user='postgres', password='eamon2hussien')
+            result = await conn.fetch(query)
+            await conn.close()
+            return result
+        except Exception as e:
+            logging.warning('FROM POSTGRES' + e)
+
+async def pubControls (data):
+    try:
+        return await redis.publish("Plant::Controls", data)
+    except Exception as e:
+        logging.warning(e)
 
 @app.route('/')
 async def base():
@@ -48,26 +49,6 @@ async def base():
 
 @app.route('/Dashboard')
 async def dashboard():
-#    try:
-#        data = await redis.get("Plant::Data")
-#        if data is not None:
-#            logging.info(json.loads(data.decode('utf-8')))
-#    except Exception as e:
-#        logging.info(e)
-# here we need a graph/
-    return render_template('Dashboard.html')
-
-@app.route('/Cards')
-async def cards():
-    return render_template('cards.html')
-
-@app.route('/database')
-async def datab():
-    result = await Pgfetch('''SELECT * FROM hydro.hydrotable order by ID''')
-    return render_template("database.html", result=result)
-
-@app.route('/ControlPanel', methods=['GET', 'POST'])
-async def index():
     PH_Reading = await Pgfetch('''SELECT READING_PH FROM hydro.hydrotable LIMIT 1''')
     PHREADING = re.findall("\=(.*)\>", str(PH_Reading))
     logging.warning(PH_Reading)
@@ -78,42 +59,155 @@ async def index():
     logging.warning(EC_Reading)
     logging.warning(ECREADING)
 
+#    try:
+#        data = await redis.get("Plant::Data")
+#        if data is not None:
+#            logging.info(json.loads(data.decode('utf-8')))
+#    except Exception as e:
+#        logging.info(e)
+# here we need a graph/
+    return render_template('card.html')
+
+
+@app.route("/superplot", methods=["GET"])
+async def plotView():
+    timeInterval = 7;
+    query = ('''SELECT TIMEZ, count(DISTINCT(READING_PH))
+                      from hydro.hydrotable
+                      WHERE TIMEZ >
+                      (current_timestamp - INTERVAL '7 days')
+                      GROUP BY TIMEZ ORDER BY TIMEZ;''')
+    try:
+        result = await Pgfetch(query)
+    except Exception as e:
+        logging.warning(e)
+
+    for rec in result:
+        tses.append(rec[0])
+        ucounts.append(rec[1])
+
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    axis.set_title("PH vs TIME")
+    axis.set_xlabel("TIME")
+    axis.set_ylabel("PH")
+    axis.grid()
+
+    # axis.plot(range(5), range(5), "ro-")
+    axis.plot(tses, ucounts, color='blue', label="ro-")#User Count
+
+    # Convert plot to PNG image
+    pngImage = io.BytesIO()
+    FigureCanvas(fig).print_png(pngImage)
+
+    # Encode PNG image to base64 string
+    pngImageB64String = "data:image/png;base64,"
+    pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
+    return render_template("plot.html", image=pngImageB64String, data=ucounts)
+
+
+@app.route('/Plot')
+async def plot():
+    timeInterval = 7;
+    ucounts = []
+    query ='''SELECT tstampz, count(DISTINCT(id))
+                from hydro
+                WHERE tstampz >
+                (current_timestamp - INTERVAL '7 days')
+                GROUP BY tstampz ORDER BY tstampz;'''
+    result = Pgfetch(query)
+    for rec in result:
+        tses.append(rec[0])
+        ucounts.append(rec[1])
+
+    ax.plot(tses, ucounts, color='blue', label='User Count')
+
+    return render_template('Dashboard.html')
+
+    # Plot = await Pgfetch('''SELECT tstampz, count(DISTINCT(id))
+    #             from hydro
+    #             WHERE tstampz >
+    #             (current_timestamp - INTERVAL '7 days')
+    #             GROUP BY tstampz ORDER BY tstampz;''')
+
+@app.route('/Cards')
+async def cards():
+    return render_template('cards.html')
+
+@app.route('/database')
+async def datab():
+    result = await Pgfetch('''SELECT * FROM hydro.hydrotable order by ID desc LIMIT 100''')
+    return render_template("database.html", result=result)
+
+
+
+@app.route('/ControlPanel', methods=['GET', 'POST'])
+async def index():
     if request.method == 'POST':
         if request.form.get('PH_ON') == 'PH_ON':
-            await redis.publish("Plant::Control", '{"W":"true"}')
-            res_dct = await fetch('/PHon')
-            logging.warning(res_dct)
+            await pubControls('/PHon')
 
         elif request.form.get('PH_OFF') == 'PH_OFF':
-            await redis.publish("Plant::Controls", '{"action": "POFF"}')
-            res_dct = await fetch('/PHoff')
-            logging.warning(res_dct)
+            await pubControls('/PHoff')
 
-        elif request.form.get('EC_ON') == 'EC_ON':
-            await redis.publish("Plant::Controls", '{"action": "PHdownON"}')
+        elif request.form.get('PH_READ') == 'PH_READ':
+            await pubControls('/Tick')
 
-        elif request.form.get('EC_OFF') == 'EC_OFF':
-            await redis.publish("Plant::Controls", '{"action": "EC_OFF"}')
+        elif request.form.get('EC_ON') == 'ON':
+            await pubControls('/ECon')
 
-        elif request.form.get('PHdown_OFF') == 'PHdown_OFF':
-            await redis.publish("Plant::Controls", '{"action": "PHdownOFF"}')
+        elif request.form.get('EC_OFF') == 'OFF':
+            await pubControls('/ECoff')
 
-        elif request.form.get('EC_ON') == 'EC_ON':
-            await redis.publish("Plant::Controls", '{"action": "ECON"}')
+        elif request.form.get('EC_READ') == 'READ':
+            await pubControls('/ECRead')
 
-        elif request.form.get('EC_OFF') == 'EC_OFF':
-            await redis.publish("Plant::Controls", '{"action": "ECOFF"}')
+        elif request.form.get('TEMP_ON') == 'TEMP_ON':
+            await pubControls('/TEMPon')
 
-        elif request.form.get('TempSensor_ON') == 'TempSensor_ON':
-            await redis.publish("Plant::Controls", '{"action": "TempON"}')
+        elif request.form.get('TEMP_OFF') == 'TEMP_OFF':
+            await pubControls('/TEMPoff')
 
-        elif request.form.get('TempSensor_OFF') == 'TempSensor_OFF':
-            await redis.publish("Plant::Controls", '{"action": "TempOFF"}')
+        elif request.form.get('TEMP_READ') == 'TEMP_READ':
+            await pubControls('/TEMPRead')
+
+        elif request.form.get('WL_ON') == 'WL_ON':
+            await pubControls('/WLon')
+
+        elif request.form.get('WL_OFF') == 'WL_OFF':
+            await pubControls('/WLoff')
+
+        elif request.form.get('WL_READ') == 'WL_READ':
+            await pubControls('/WLRead')
+
+        elif request.form.get('MPUMP_ON') == 'MPUMP_ON':
+            await pubControls('/MPUMPon')
+
+        elif request.form.get('MPUMP_OFF') == 'MPUMP_OFF':
+            await pubControls('/MPUMPoff')
+
+        elif request.form.get('ECUP_ON') == 'ECUP_ON':
+            await pubControls('/ECUPon')
+
+        elif request.form.get('ECUP_OFF') == 'ECUP_OFF':
+            await pubControls('/ECUPoff')
+
+        elif request.form.get('PHUP_ON') == 'PHUP_ON':
+            await pubControls('/PHUPon')
+
+        elif request.form.get('PHUP_OFF') == 'PHUP_OFF':
+            await pubControls('/PHUPoff')
+
+        elif request.form.get('PHDWN_ON') == 'PHDWN_ON':
+            await pubControls('/PHDWNon')
+
+        elif request.form.get('PHDWN_OFF') == 'PHDWN_OFF':
+            await pubControls('/PHDWNoff')
 
     elif request.method == 'GET':
         print("No Post Back Call")
-
-    return render_template("index.html", PHREADING=PHREADING , ECREADING=ECREADING)
+    return render_template("index.html", PHREADING=101 , ECREADING=101)
 
 
 if __name__ == '__main__':
