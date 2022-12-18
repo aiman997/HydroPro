9f3a44590dd3d02a8746a59fd10c7ae70b547df7
mahrous-amer <mahrous-amer@protonmail.com>
2022-12-18 22:25:51 +0800

    rpi init

--- subscriber/sub.py
+++ subscriber/sub.py
@@ -1,54 +1,75 @@
-import os
-import sys
-import time
-import re
-import json
 import asyncio
-import logging
-import aiohttp
-import aioredis
 import async_timeout
+import time
+import json
+import logging
+import redis.asyncio as redis
+import aiohttp
+import websockets
+from dotenv import load_dotenv
 
-URL = 'http://10.243.199.34:8000'
-redis = aioredis.from_url("redis://redis", db=1)
-pubsub = redis.pubsub()
+load_dotenv()
+
+PREFIX = "HYDRO::PLANT"
+IP = os.environ.get('IP')
+PORT = os.environ.get('PORT')
+READING_DURATION = '15'
+
 logging.basicConfig(level=logging.INFO)
-STOPWORD = "STOP"
 
-async def fetch(route):
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with async_timeout.timeout(2):
-                resp = await session.get(URL+route)
-                response = await resp.text()
-                if response is not None:
-                    return response
-        except Exception as e:
-            logging.warning('ERROR WHILE FETCHING DATA: '+str(e))
+class RPI:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.ws_url = f"ws://{ip}:{port}/ws/"
+        self.redis = redis.Redis(host='redis', port=6379, decode_responses=True)
+        self.channel_name = f"{PREFIX}::{ip}::CONTROLS"
 
-async def reader(channel: aioredis.client.PubSub):
-    while True:
-        try:
-            async with async_timeout.timeout(1):
-                message = await channel.get_message(ignore_subscribe_messages=True)
-                if message is not None:
-                    if message["data"].decode() == STOPWORD:
-                        logging.info('Dead: '+message["data"].decode())
-                        break
-                    else:
-                        logging.info('Got: '+message["data"].decode())
-                        return await fetch(message["data"].decode())
-                await asyncio.sleep(0.1)
-        except asyncio.TimeoutError:
-            await redis.publish("Plant::Controls", STOPWORD)
-            pass
+    async def publish_readings(self):
+        readings['time'] = time.time()
+        logging.info(f"Publishing readings at {time.time()}: {readings}")
+        await self.redis.xadd(name=f"{PREFIX}::{ip}::READINGS", fields=readings, id='*', maxlen=100, approximate=True, nomkstream=False, minid=None, limit=None)
 
-async def main():
-    await pubsub.psubscribe("Plant::Controls")
-    future = asyncio.create_task(reader(pubsub))
-    await future
+    async def handel_readings(self, sensors):
+        ws = await websockets.connect(f'ws://{self.ip}:{self.port}/ws/all')
+        while True:
+            logging.info(f"Trying readings...")
+            try:
+                await ws.send(READING_DURATION)
+                async with async_timeout.timeout(int(READING_DURATION)):
+                    res = await ws.recv()
+                    readings = json.loads(res)
+                    await self.publish_readings(readings)
+            except (asyncio.TimeoutError, json.decoder.JSONDecodeError) as e:
+                logging.error(e)
+            await asyncio.sleep(10)
+
+    async def handel_controls(self):
+        pubsub = self.redis.pubsub()
+        await pubsub.subscribe(self.channel_name+'::AUTOMATIC', self.channel_name+'::MANUAL')
+        while True:
+            logging.info(f"Trying controls...")
+            try:
+                async with async_timeout.timeout(1):
+                    message = await pubsub.get_message()
+                    if message and message["type"] == "message":
+                        payload = json.loads(message["data"])
+                        # TODO: async def validate_control()
+                        logging.debug(f"Payload is: {payload}")
+                        await self.publish_control(payload["param"], payload["duration"])
+            except (asyncio.TimeoutError, json.decoder.JSONDecodeError) as e:
+                logging.error(f"Error while handeling controls: {e}")
+            await asyncio.sleep(10)
+
+    async def publish_control(param: str, duration: int):
+        logging.debug(f"Sending message to control {param}: {duration}")
+        await asyncio.sleep(0.2)
 
 if __name__ == "__main__":
-    asyncio.run(main())
-
-
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    pi = RPI(IP, PORT)
+    logging.info(f"Starting...")
+    loop.create_task(pi.handel_controls())
+    loop.create_task(pi.handel_readings())
+    loop.run_forever()
