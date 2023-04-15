f3c6d99d5915acf1c8fd84f4dc6132cb1443a2ef
mahrous-amer <mahrous-amer@protonmail.com>
2023-04-16 02:01:55 +0800

    wip sub wsapi

--- wsapi/main.py
+++ wsapi/main.py
@@ -5,14 +5,14 @@ import bcrypt
 from datetime import datetime, timezone, timedelta
 from typing import Union, Optional
 from fastapi import (
-	Cookie,
-	Depends,
-	FastAPI,
-	Query,
-	WebSocket,
-	WebSocketException,
-	status,
-	WebSocketDisconnect,
+    Cookie,
+    Depends,
+    FastAPI,
+    Query,
+    WebSocket,
+    WebSocketException,
+    status,
+    WebSocketDisconnect,
 )
 from fastapi.responses import HTMLResponse
 from fastapi.middleware import Middleware
@@ -48,29 +48,18 @@ pubsub = redis.pubsub()
 
 # Utils
 
-def validate_json_request(json_request):
-	try:
-		request = json.loads(json_request)
-		data    = json.loads(request['data'])
-		if "type" in request and "data" in request:
-			return True
-		else:
-			return False
-	except Exception as e:
-		logging.error(f"Invalid request with error: {e}")
-		return False
 
-def validate_auth_request(json_request):
-	try:
-		request = json.loads(json_request)
-		data    = json.loads(request['data'])
-		if "type" in request and "data" in request:
-			return True
-		else:
-			return False
-	except Exception as e:
-		logging.error(f"Invalid request with error: {e}")
-		return False
+def validate_json_request(json_request):
+    try:
+        request = json.loads(json_request)
+        data = json.loads(request['data'])
+        if "type" in request and "data" in request:
+            return True
+        else:
+            return False
+    except Exception as e:
+        logging.error(f"Invalid request with error: {e}")
+        return False
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
@@ -82,6 +71,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
     return encoded_jwt
 
+
 def decode_access_token(token: str):
     try:
         decoded_token = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
@@ -91,79 +81,92 @@ def decode_access_token(token: str):
     except jwt.InvalidTokenError:
         return None
 
+
 async def get_cookie_or_token(
-	websocket: WebSocket,
-	session: Union[str, None] = Cookie(default=None),
-	token: Union[str, None] = Query(default=None),
+        websocket: WebSocket,
+        session: Union[str, None] = Cookie(default=None),
+        token: Union[str, None] = Query(default=None),
 ):
-	if session is None and token is None:
-		raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
-	return session or token
+    if session is None and token is None:
+        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
+    return session or token
+
 
 async def send_event(stream, action, data={}):
-	try:
-		event = Event(stream=stream, action=action, data=data)
-		await event.publish(redis)
-		logging.info("success")
-	except Exception as e:
-		logging.error(f"failied with error: {e}")
+    try:
+        event = Event(stream=stream, action=action, data=data)
+        await event.publish(redis)
+        logging.info("success")
+    except Exception as e:
+        logging.error(f"failied with error: {e}")
 
 # Routes
 
+
 @app.get("/")
 async def get():
-	with open(os.path.join(root, 'index.html')) as fh:
-		html = fh.read()
-	return HTMLResponse(html)
+    with open(os.path.join(root, 'index.html')) as fh:
+        html = fh.read()
+    return HTMLResponse(html)
+
 
 @app.get("/health")
 async def get():
-	return "OK"
+    return "OK"
 
-@app.get("/token")
-async def get():
-	token = create_access_token(dict())
-	return token
 
-"""
-{
-	"type": "action",
-	"data": "{
-		"action": "newuser",
-		"args":{
-			"first_name": "blablabla"
-		}"
-	}
-}
-"""
+@app.post("/auth")
+async def auth():
+    token = create_access_token(dict())
+    return token
+
+
+@app.post("/auth")
+async def authenticate_user(email: str, password: str):
+    await send_event('api', 'authuser', json.loads({'email': email, 'password': password}))
+    if not validate_credentials(email, password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password")
+    # Generate access token
+    token = create_access_token({"email": email})
+    return {"access_token": token}
+
+
 @app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket, q: Union[int, None] = None, cookie_or_token: str = Depends(get_cookie_or_token) ):
-	try:
-		await manager.connect(websocket)
-		while True:
-			request = await websocket.receive_text()
-			if not validate_json_request(request) or not validate_auth_request(request):
-				await manager.send_message(json.dumps({"echo_request": request, "error": "Invalid json request"}), websocket)
-			decoded_token = decode_access_token(cookie_or_token)
-			logging.info(decoded_token + '   ' +cookie_or_token)
-			if decoded_token is None:
-					raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
-			await pubsub.subscribe(decoded_token)
-			request = json.loads(request)
-			data = json.loads(request['data'])
-			args = data['args']
-			args['auth'] = decoded_token
-			await send_event('api', data['action'], args)
-			try:
-				res = await asyncio.wait_for(pubsub.get_message(ignore_subscribe_messages=True, timeout=30), timeout=30.0)
-				if res is not None:
-					# Check rate limit before sending messages
-					await manager.send_message(json.dumps({"echo_request": data, "response": res}), websocket)
-				else:
-					await manager.send_message(json.dumps({"echo_request": request, "error": "No response"}), websocket)
-				await pubsub.unsubscribe(decoded_token)
-			except TimeoutError:
-					logging.info('Timeout!')
-	except Exception as e:
-		logging.info(f"Disconecting ws client due to error: {e}")
-		manager.disconnect(websocket)
+async def websocket_endpoint(websocket: WebSocket, q: Union[int, None] = None, cookie_or_token: str = Depends(get_cookie_or_token)):
+    try:
+        await manager.connect(websocket)
+        while True:
+            try:
+                request = await websocket.receive_text()
+                if not validate_json_request(request):
+                    await manager.send_message(json.dumps({"echo_request": request, "error": "Invalid json request"}), websocket)
+                    continue
+                request = json.loads(request)
+                data = json.loads(request['data'])
+                args = data['args']
+                decoded_token = decode_access_token(cookie_or_token)
+                ch = manager.get_channel_name(websocket)
+                if ch:
+                    args['auth'] = ch
+                if decoded_token is None:
+                    logging.info('Invalid token')
+                    # raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
+                await send_event('api', data['action'], args)
+                try:
+                    res = await asyncio.wait_for(manager.receive_message(websocket), timeout=30.0)
+                    if res is not None:
+                        await manager.send_message(json.dumps({"echo_request": data, "response": res}), websocket)
+                    else:
+                        await manager.send_message(json.dumps({"echo_request": request, "error": "No response"}), websocket)
+                except TimeoutError:
+                    logging.warning('Timeout while waiting for response!')
+            except WebSocketDisconnect:
+                logging.info('Client disconnected')
+                break
+            except Exception as e:
+                logging.error(f"Error while processing the message: {e}")
+    except Exception as e:
+        logging.error(f"Disconecting ws client due to error: {e}")
+    finally:
+        await manager.disconnect(websocket)
--- wsapi/manger.py
+++ wsapi/manger.py
@@ -1,36 +1,77 @@
-from typing import List
+import asyncio
+import logging
+import json
+import uuid
+from typing import List, Union
+
+import redis.asyncio as redis
 from fastapi import WebSocket, WebSocketDisconnect
 
 MAX_CONNECTIONS = 100
 
+logging.basicConfig(level=logging.DEBUG)
+
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[WebSocket] = []
-    
+        self.active_connections = {}
+        self.redis = redis.Redis(host='redis', port=6379, decode_responses=True)
+        self.pubsub = self.redis.pubsub(ignore_subscribe_messages=True)
+
     async def connect(self, websocket: WebSocket):
         try:
             if len(self.active_connections) >= MAX_CONNECTIONS:
                 await websocket.close(code=1000, reason="Too many connections")
             else:
                 await websocket.accept()
-                self.active_connections.append(websocket)
+                channel_name = str(uuid.uuid4())
+                # Cache the channel name in Redis for 5 minutes
+                await self.redis.set(channel_name, websocket.client.host, ex=300)
+                self.active_connections[websocket] = channel_name
         except WebSocketDisconnect:
-            self.active_connections.remove(websocket)
+            del self.active_connections[websocket]
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    async def disconnect(self, websocket: WebSocket):
+        channel_name = self.active_connections.pop(websocket, None)
+        if channel_name is not None:
+            await self.redis.delete(channel_name)
 
     async def send_message(self, message: str, websocket: WebSocket):
         try:
             await websocket.send_text(message)
         except Exception as e:
-            logger.error(f"Error sending message: {e}")
+            logging.error(f"Error sending message: {e}")
             await websocket.close(code=1011, reason="Internal server error")
 
     async def broadcast(self, message: str):
-        for connection in self.active_connections:
+        for connection, channel_name in self.active_connections.items():
             try:
                 await connection.send_text(message)
             except Exception as e:
-                logger.error(f"Error broadcasting message: {e}")
-                self.active_connections.remove(connection)
+                logging.error(f"Error broadcasting message: {e}")
+                del self.active_connections[connection]
+                self.redis.delete(channel_name)
+
+    async def publish_message(self, message: str):
+        try:
+            await self.pubsub.publish(self.channel, message)
+        except Exception as e:
+            logging.error(f"Error publishing message: {e}")
+
+    async def receive_message(self, websocket: WebSocket):
+        try:
+            channel_name = self.active_connections[websocket]
+            await self.pubsub.subscribe(channel_name)
+            while True:
+                message = await self.pubsub.get_message()
+                if message and message["type"] == "message":
+                    return message["data"]
+                else:
+                    continue
+        except Exception as e:
+            logging.error(f"Error receiving message: {e}")
+            return None
+        finally:
+            await self.pubsub.unsubscribe(channel_name)
+
+    def get_channel_name(self, websocket: WebSocket) -> Union[str, None]:
+        return self.active_connections.get(websocket)
